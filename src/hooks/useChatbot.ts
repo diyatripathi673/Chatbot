3040d170976fafe5ff7657dcd3a66ed106d26d64
Diya Tripathi <tripathidiya673@gmail.com>
2026-03-18 13:34:17 +0530

    env add

--- src/hooks/useChatbot.ts
+++ src/hooks/useChatbot.ts
@@ -18,6 +18,8 @@ const useChatbot = () => {
     setMessages(newMessages);
 
   try {
+    console.log(import.meta.env.VITE_OPENROUTER_API_KEY);
+
  const response = await axios.post(
   "https://openrouter.ai/api/v1/chat/completions",
   {
@@ -25,14 +27,16 @@ const useChatbot = () => {
     messages: [{ role: "user", content: message }],
   },
   {
-    headers: {
-      Authorization: `Bearer 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`,
-      "Content-Type": "application/json",
-    },
+   headers: {
+  Authorization: `Bearer ${import.meta.env.VITE_OPENROUTER_API_KEY}`,
+  "Content-Type": "application/json",
+  "HTTP-Referer": "http://localhost:3000",
+  "X-Title": "Chatbot App",
+}
   }
 );
-
   console.log("FULL RESPONSE 👉", response.data); // 🔍 debug
+console.log(import.meta.env.VITE_OPENROUTER_API_KEY);
 
   const botMessage =
     response.data?.choices?.[0]?.message?.content ||
